2a7309c10926041df766ea658cbe2b81c37ecc67
lihaiyang2046 <1160362008@qq.com>
2018-12-24 09:54:26 +0800

    新增用户查询接口

--- huawei/src/main/java/com/huawei/huawei/controller/UserController.java
+++ huawei/src/main/java/com/huawei/huawei/controller/UserController.java
@@ -2,18 +2,22 @@ package com.huawei.huawei.controller;
 import com.huawei.huawei.model.User;
 import com.huawei.huawei.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
+@RequestMapping("/api/user")
 @RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/person/save")
+    /**
+     * 保存用户
+     * @param name
+     * @return
+     */
+    @PostMapping("/save")
     public User save(@RequestParam String name)
     {
         User user = new User();
@@ -22,6 +26,19 @@ public class UserController {
         {
             System.out.printf("用户对象：%s 保存成功！\n", user);
         }
+        return user;
+    }
+
+    /**
+     * 根据用户ID查询用户信息
+     */
+    @RequestMapping("/getUserByUserID")
+    @ResponseBody
+    public User getUserByUserID(int userId) {
+        User user = new User();
+        user.setID(userId);
+        user.setUserName("Tom");
+
         return user;
     }
 
